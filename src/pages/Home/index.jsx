8c1fcdbcb1e5f36576ad4b22117d9562cc19ae42
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2024-11-07 09:43:07 -0300

    Integrando com o back-end

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,51 +1,77 @@
+import { useEffect, useState, useRef } from "react";
 import "./style.css";
 import Trash from "../../assets/trash.svg";
+import api from "../../services/api";
 
 function Home() {
-  const users = [
-    {
-      id: "23udfhudf232",
-      name: "Rodolfo",
-      age: 33,
-      email: "rod@email.com",
-    },
-    {
-      id: "23ufdsdgdgudf232",
-      name: "Aline",
-      age: 28,
-      email: "aline@email.com",
-    },
-    {
-      id: "23ufgfgfgdgudf232",
-      name: "Paulo",
-      age: 66,
-      email: "paulo@email.com",
-    },
-  ];
+  const [users, setUsers] = useState([]);
+
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  async function getUsers() {
+    const usersFromApi = await api.get("/usuarios");
+
+    setUsers(usersFromApi.data);
+    console.log(users);
+  }
+
+  async function createUsers() {
+    await api.post("/usuarios", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value,
+    });
+
+    getUsers();
+  }
+
+  async function deleteUsers(id) {
+    await api.delete(`/usuarios/${id}`);
+
+    getUsers();
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return (
     <div className="container">
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name="name" type="text" />
-        <input nome="age" type="number" />
-        <input name="email" type="email" />
-        <button type="button">Cadastrar</button>
+        <input placeholder="Nome" name="name" type="text" ref={inputName} />
+        <input placeholder="Idade" nome="age" type="number" ref={inputAge} />
+        <input
+          placeholder="E-mail"
+          name="email"
+          type="email"
+          ref={inputEmail}
+        />
+        <button type="button" onClick={createUsers}>
+          Cadastrar
+        </button>
       </form>
 
       {users.map((user) => (
-        <div key={user.id}>
+        <div key={user.id} className="card">
           <div>
-            <p>Nome: {user.name} </p>
-            <p>Idade: {user.age}</p>
-            <p>E-mail: {user.email}</p>
+            <p>
+              Nome: <span>{user.name}</span>{" "}
+            </p>
+            <p>
+              Idade: <span>{user.age}</span>
+            </p>
+            <p>
+              E-mail: <span>{user.email}</span>
+            </p>
           </div>
-          <button>
+          <button onClick={() => deleteUsers(user.id)}>
             <img src={Trash} />
           </button>
         </div>
       ))}
-
     </div>
   );
 }
